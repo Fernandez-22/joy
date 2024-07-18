cbe67cda523f958da528a7324e7f92670e8dff88
Erica Joy Fernandez <fernandezea.228.stud.@cdd.edu.ph>
2024-07-18 16:47:41 +0800

    hello

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" contents="ie=edge">
+    <title>Erica Joy Fernandez</title>
+</head>
+<body>
+    <h1>ITP17 ADVANCE PROGRAMMING</h1>
+
+    <a href="{{ url('/post')}}">POST</a>
+    
+</body>
+</html>
